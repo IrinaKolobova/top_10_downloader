50000c388948ef109d0a36d142ef914843e6c541
Irina Kolobova <kolobovairinav@gmail.com>
2020-04-28 17:23:29 -0700

    Initial commit

---
+++ app/src/main/java/learnprogramming/academy/MainActivity.kt
@@ -0,0 +1,41 @@
+package learnprogramming.academy
+
+import android.os.AsyncTask
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate called")
+        val downloadData = DownloadData()
+        downloadData.execute("URL goes here")
+        Log.d(TAG, "onCreate: done")
+    }
+
+    companion object{
+        private class DownloadData : AsyncTask<String, Void, String>(){
+            private val TAG = "DownloadData"
+
+            override fun onPostExecute(result: String?) {
+                super.onPostExecute(result)
+                Log.d(TAG, "onPostExecute: parameter is $result")
+            }
+
+            override fun doInBackground(vararg url: String?): String {
+                Log.d(TAG, "doInBackground: starts with ${url[0]}")
+                val rssFeed = downloadXML(url[0])
+                if(rssFeed.isEmpty()){
+                    Log.e(TAG, "doInBackground: Error downloading")
+                }
+                return rssFeed
+            }
+
+    }
+    }
+}
