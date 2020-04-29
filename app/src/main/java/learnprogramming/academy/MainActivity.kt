d427f80e9e5ba4cc096371b8f57ac37b22610f29
Irina Kolobova <kolobovairinav@gmail.com>
2020-04-28 23:55:46 -0700

    Refactor download function

--- app/src/main/java/learnprogramming/academy/MainActivity.kt
+++ app/src/main/java/learnprogramming/academy/MainActivity.kt
@@ -47,37 +47,26 @@ class MainActivity : AppCompatActivity() {
             private fun downloadXML(urlPath: String?): String{
                 val xmlResult = StringBuilder()
 
-                try{
+                try {
                     val url = URL(urlPath)
                     val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
                     val response = connection.responseCode
                     Log.d(TAG, "downloadXML: The response code was $response")
 
-                    val reader = BufferedReader(InputStreamReader(connection.inputStream))
-
-                    val inputBuffer = CharArray(500)
-                    var charsRead = 0
-                    while (charsRead >= 0 ){
-                        charsRead = reader.read(inputBuffer)
-                        if(charsRead > 0) {
-                            xmlResult.append(String(inputBuffer, 0, charsRead))
-                        }
-                    }
-                    reader.close()
+                    connection.inputStream.buffered().reader().use {xmlResult.append(it.readText())}
 
                     Log.d(TAG, "Received ${xmlResult.length} bytes")
                     return xmlResult.toString()
-
-                } catch (e: MalformedURLException){
-                    Log.e(TAG, "downloadXML: Invalid URL ${e.message}")
-                } catch (e: IOException) {
-                    Log.e(TAG, "downloadXML: IO Exception reading data: ${e.message}")
-                } catch (e: SecurityException){
-                    Log.e(TAG, "downloadXML: Security Exception. Needs permissions? ${e.message}")
-                } catch(e: Exception){
-                    Log.e(TAG, "Unknown error: ${e.message}")
+                } catch (e: Exception){
+                    val errorMessage: String = when(e){
+                        is MalformedURLException -> "downloadXML: Invalid URL ${e.message}"
+                        is IOException -> "downloadXML: IO Exception reading data: ${e.message}"
+                        is SecurityException -> { e.printStackTrace()
+                            "downloadXML: Security Exception. Needs permission? ${e.message}"
+                        }
+                        else -> "Unknown error: ${e.message}"
+                    }
                 }
-
                 return "" // If it gets here, there's been a problem. Return an empty string
             }
     }
