525b6fb47c1cb8dec640e4395d4c71a8855165ff
Irina Kolobova <kolobovairinav@gmail.com>
2020-04-29 00:03:17 -0700

    Refactor download function to max

--- app/src/main/java/learnprogramming/academy/MainActivity.kt
+++ app/src/main/java/learnprogramming/academy/MainActivity.kt
@@ -4,13 +4,6 @@ import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import java.io.BufferedInputStream
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.InputStreamReader
-import java.lang.Exception
-import java.net.HttpURLConnection
-import java.net.MalformedURLException
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
@@ -26,8 +19,8 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "onCreate: done")
     }
 
-    companion object{
-        private class DownloadData : AsyncTask<String, Void, String>(){
+    companion object {
+        private class DownloadData : AsyncTask<String, Void, String>() {
             private val TAG = "DownloadData"
 
             override fun onPostExecute(result: String?) {
@@ -38,38 +31,16 @@ class MainActivity : AppCompatActivity() {
             override fun doInBackground(vararg url: String?): String {
                 Log.d(TAG, "doInBackground: starts with ${url[0]}")
                 val rssFeed = downloadXML(url[0])
-                if(rssFeed.isEmpty()){
+                if (rssFeed.isEmpty()) {
                     Log.e(TAG, "doInBackground: Error downloading")
                 }
                 return rssFeed
             }
 
-            private fun downloadXML(urlPath: String?): String{
-                val xmlResult = StringBuilder()
-
-                try {
-                    val url = URL(urlPath)
-                    val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
-                    val response = connection.responseCode
-                    Log.d(TAG, "downloadXML: The response code was $response")
-
-                    connection.inputStream.buffered().reader().use {xmlResult.append(it.readText())}
-
-                    Log.d(TAG, "Received ${xmlResult.length} bytes")
-                    return xmlResult.toString()
-                } catch (e: Exception){
-                    val errorMessage: String = when(e){
-                        is MalformedURLException -> "downloadXML: Invalid URL ${e.message}"
-                        is IOException -> "downloadXML: IO Exception reading data: ${e.message}"
-                        is SecurityException -> { e.printStackTrace()
-                            "downloadXML: Security Exception. Needs permission? ${e.message}"
-                        }
-                        else -> "Unknown error: ${e.message}"
-                    }
-                }
-                return "" // If it gets here, there's been a problem. Return an empty string
+            private fun downloadXML(urlPath: String?): String {
+                return URL(urlPath).readText()
             }
-    }
+        }
     }
 
 }
